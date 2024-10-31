0bc7e65bf5372599dc2c25f1b3bdf58e8d89231c
BingYang <1468947017@qq.com>
2024-10-31 20:56:47 +0800

    fix lauch

--- diverse_lauch/src/main.rs
+++ diverse_lauch/src/main.rs
@@ -31,23 +31,26 @@ fn main() {
     let json_path = exec_path.parent().unwrap().join("Dependencies.json");
     if json_path.exists()  {
         let json_str = std::fs::read_to_string(json_path.clone()).unwrap();
-        let json: serde_json::Value = serde_json::from_str(&json_str).unwrap();
-        let dependencies = json["dependencies"].as_array().unwrap();
-        for dep in dependencies.iter() {
-            //whether dep["name"].as_str() == "torch"
-            if dep["name"].as_str().unwrap() == "torch" {
-                torch_url = dep["url"].as_str().unwrap().to_string();
-            }else{
-                let outpath = dep["output"].as_str().unwrap();
-                if !Path::new(outpath).exists() {
-                    let mut command = Command::new(exec_dir_path.join("diverseupdate.exe"));
-                    command.arg(format!("{}",dep["name"].as_str().unwrap()));
-                    command.arg(format!("{}",dep["url"].as_str().unwrap()));
-                    command.arg(format!("{}",outpath));
-                    command.current_dir(exec_dir_path.as_path());
-                    let child = command.spawn().unwrap();
-                    let output = child.wait_with_output().unwrap();
-                    println!("{}", String::from_utf8_lossy(&output.stdout));
+        let json : Result<serde_json::Value, serde_json::Error>= serde_json::from_str(&json_str);
+        if json.is_ok() {
+            let json = json.unwrap();
+            let dependencies = json["dependencies"].as_array().unwrap();
+            for dep in dependencies.iter() {
+                //whether dep["name"].as_str() == "torch"
+                if dep["name"].as_str().unwrap() == "torch" {
+                    torch_url = dep["url"].as_str().unwrap().to_string();
+                }else{
+                    let outpath = dep["output"].as_str().unwrap();
+                    if !Path::new(outpath).exists() {
+                        let mut command = Command::new(exec_dir_path.join("diverseupdate.exe"));
+                        command.arg(format!("{}",dep["name"].as_str().unwrap()));
+                        command.arg(format!("{}",dep["url"].as_str().unwrap()));
+                        command.arg(format!("{}",outpath));
+                        command.current_dir(exec_dir_path.as_path());
+                        let child = command.spawn().unwrap();
+                        let output = child.wait_with_output().unwrap();
+                        println!("{}", String::from_utf8_lossy(&output.stdout));
+                    }
                 }
             }
         }
@@ -66,27 +69,30 @@ fn main() {
         let version_file = exec_dir_path.join("Version.json");
         if version_file.exists() {
             let version_str = fs::read_to_string(version_file).unwrap();
-            let version_json: serde_json::Value = serde_json::from_str(&version_str).unwrap();
-            if !version_json["dependencies"].is_null(){
-                let deps = version_json["dependencies"].as_array().unwrap();
-                // println!("version_dep: {}", version_dep);
-                for dep in deps.iter() {
-                    if dep["name"].as_str().unwrap() == "torch" {
-                        let version_dep = dep["url"].as_str().unwrap().to_string();
-                        if version_dep != torch_url {
-                            torch_url = version_dep;
-                            need_install_dep = true;
+            let version_json: Result<serde_json::Value, serde_json::Error> = serde_json::from_str(&version_str);
+            if version_json.is_ok() {
+                let version_json = version_json.unwrap();
+                if !version_json["dependencies"].is_null(){
+                    let deps = version_json["dependencies"].as_array().unwrap();
+                    // println!("version_dep: {}", version_dep);
+                    for dep in deps.iter() {
+                        if dep["name"].as_str().unwrap() == "torch" {
+                            let version_dep = dep["url"].as_str().unwrap().to_string();
+                            if version_dep != torch_url {
+                                torch_url = version_dep;
+                                need_install_dep = true;
+                            }
                         }
                     }
-                }
-                  //write the version to the dependencies.json file
-                if need_install_dep {
-                    let json_str = fs::read_to_string(json_path.clone()).unwrap();
-                    let mut json: serde_json::Value = serde_json::from_str(&json_str).unwrap();
-                    json["dependencies"][0]["name"] = serde_json::Value::String("torch".to_string());
-                    json["dependencies"][0]["url"] = serde_json::Value::String(torch_url.clone());
-                    let new_json_str = serde_json::to_string_pretty(&json).unwrap();
-                    fs::write(json_path, new_json_str).unwrap();
+                    //write the version to the dependencies.json file
+                    if need_install_dep {
+                        let json_str = fs::read_to_string(json_path.clone()).unwrap();
+                        let mut json: serde_json::Value = serde_json::from_str(&json_str).unwrap();
+                        json["dependencies"][0]["name"] = serde_json::Value::String("torch".to_string());
+                        json["dependencies"][0]["url"] = serde_json::Value::String(torch_url.clone());
+                        let new_json_str = serde_json::to_string_pretty(&json).unwrap();
+                        fs::write(json_path, new_json_str).unwrap();
+                    }
                 }
             }
         }
